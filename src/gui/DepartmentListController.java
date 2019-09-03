129a909a2a0d87749e26b184e8f2d6fe7b4988d7
Rafael Pedro da Silva <rafaelpedrodasilva@outlook.com>
2019-09-03 10:46:40 -0300

    Department ComboBox

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -165,6 +165,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 
 			dialogStage.showAndWait();
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
